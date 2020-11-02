3fe89d16859586cf3e546c00b31ea1933d41cf72
jlalla <juanlalla@gmail.com>
2020-11-02 19:15:54 -0300

    Se agregan migraciones para creación de la base de datos

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Notas.Models
 {
     public class Usuario
     {
+        [Key]
         public string Mail { get; set; }
+        [Required]
         public string Nombre { get; set; }
 
         public List<Nota> Notas { get; set; }
